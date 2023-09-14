928bb572cd24348474d468ece92954665b9f02cf
AnonLuc <lucasdias@billericak12.com>
2023-09-14 09:37:18 -0400

    Initial Commit

--- Yummy.java
+++ Yummy.java
@@ -2,10 +2,22 @@ public class Yummy {
     public static void main(String[] args) {
         System.out.println("Yummy!");
         chocolate(); // call the chocolate method
-
+        frenchFries();
+        iceCream();
+        System.out.println("done!");
     }
     public static void chocolate() {
-        System.out.print("Chocolate");
+        System.out.print("Chocolate ");
         System.out.println("Cake!");
+    }
+    public static void frenchFries() {
+        System.out.println("French Fries with ketchup");
+        chocolate();
+        System.out.println("that was yummy!");
+    }
+    public static void iceCream() {
+        System.out.println("Ice");
+        frenchFries();
+        System.out.println("Cream");
     }
 }
