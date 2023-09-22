22fd355daa351040edbbe80e89e8f8ea3f7f4e13
AnonLuc <lucasdias@billericak12.com>
2023-09-22 10:23:48 -0400

    Initial Commit

--- MathEx.java
+++ MathEx.java
@@ -3,8 +3,14 @@ public class MathEx {
         int x = 123;
         int y = swap(x);
         System.out.println(y);
+        timeLeft(1, 15, 4, 36);
         timeLeft(1, 34, 8, 20);
+        System.out.println(dayOfWeek(0, 1));
         System.out.println(dayOfWeek(0, 14));
+        System.out.println(dayOfWeek(6, 22));
+        System.out.println(dayOfWeek(5, 4));
+        System.out.println(dayOfWeek(1, 24));
+        System.out.println(dayOfWeek(2, 1));
     }
     public static int swap(int x) {
         int ones   = x%100/10;
