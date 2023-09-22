558fb07903b6003e041846fb911cdb51943169a1
AnonLuc <lucasdias@billericak12.com>
2023-09-22 09:55:29 -0400

    Initial Commit

--- MathEx.java
+++ MathEx.java
@@ -4,19 +4,24 @@ public class MathEx {
         int y = swap(x);
         System.out.println(y);
         timeLeft(1, 34, 8, 20);
+        System.out.println(dayOfWeek(0, 14));
     }
     public static int swap(int x) {
-        int ones = x%100/10;
-        int tens = x/10%10;
+        int ones   = x%100/10;
+        int tens   = x/10%10;
         int result = ones*100 + tens*10;
         return result;
     }
     public static void timeLeft(int curHour, int curMin, int depHour, int depMin) {
-        int curTime = curHour*60 + curMin;
-        int depTime = depHour*60 + depMin;
+        int curTime  = curHour*60 + curMin;
+        int depTime  = depHour*60 + depMin;
         int waitTime = depTime - curTime;
         int waitHour = waitTime/60;
-        int waitMin = waitTime%60;
+        int waitMin  = waitTime%60;
         System.out.println("You will have to wait " + waitHour + " hours and " + waitMin + " minutes.");
+    }
+    public static int dayOfWeek(int jan1DayOfWeek, int date) {
+        int days = (date - 1) % 7;
+        return (days + jan1DayOfWeek) % 7;
     }
 }
