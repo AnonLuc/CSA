dc58ce6561e6e63889a30a75f98751c435c20108
AnonLuc <lucasdias@billericak12.com>
2023-10-04 09:54:12 -0400

    Initial Commit

--- Box.java
+++ Box.java
@@ -26,11 +26,29 @@ public class Box {
         color = "white";
         hasLid = true;
     }
+    //accessor methods get/set
+    public double getHeight() {
+        return height;
+    }
+    public void setHeight(double h) {
+        if(h>0){
+            height = h;
+        }
+    }
+    
     public static void main(String[] args) {
        Box b1 = new Box(5.6, 2.5 , 3.7, "purple", true);
 
        Box b2 = new Box(3.4, 1.2, 2.3);
 
        Box b3 = new Box();
+       b3.setHeight(4.5);
+       double h3 = b3.getHeight();
+       System.out.println(h3);
+       double h1 = b1.getHeight();
+       System.out.println(h1);
+       double h2 = b2.getHeight();
+       System.out.println(h2);
+    
     }
 }
