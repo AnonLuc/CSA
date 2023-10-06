b5c8e06870c724cdda05cef0783655f73a0d23fb
AnonLuc <lucasdias@billericak12.com>
2023-10-06 09:41:55 -0400

    Frog Commit

--- Frogs and Fractions/Frog.java
+++ Frogs and Fractions/Frog.java
@@ -76,13 +76,17 @@ public class Frog {
 		Frog f2 = new Frog("Francine", -2,0);
 		f2.eatFly();
 		// TODO uncomment these to test new code.
-		//f2.jump(2);
-		//f1.visit(f2);
+		f2.jump(2);
+		f1.visit(f2);
 		System.out.println(f1);
 		System.out.println(f2);
 		
 		// make a new frog named Frank.  have him jump twice and then go visit Freddy.
-
+		Frog f3 = new Frog("Frank", 0, 0);
+		f3.jump(2);
+		f3.jump(2);
+		f3.visit(f1);
+		System.out.println(f3);
 	}
 
 }
