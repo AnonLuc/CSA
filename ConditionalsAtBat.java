0b3d18862b2f6ffa9c9fcfbec241d94b5dd22878
AnonLuc <lucasdias@billericak12.com>
2023-11-01 10:30:57 -0400

    atBat Classwork

--- ConditionalsAtBat.java
+++ ConditionalsAtBat.java
@@ -11,24 +11,49 @@ public class ConditionalsAtBat {
         return (!negative && a < 0 ^ b < 0);
     }
 
+    public boolean pickUpPhone(boolean isMorning, boolean isMom, boolean isAsleep){
+        if(isMorning && isMom && !isAsleep){
+            return true;
+        }
+        return !(isMorning || isAsleep);
+    }
+
 
     public static void main(String[] args) {
-        ConditionalsAtBat conditionals = new ConditionalsAtBat();
+        ConditionalsAtBat condition = new ConditionalsAtBat();
 
         // Question 1
         System.out.println("Question 1: \n");
-        System.out.println(conditionals.chimpTrouble(true, true));
-        System.out.println(conditionals.chimpTrouble(false, false));
-        System.out.println(conditionals.chimpTrouble(true, false));
-        System.out.println(conditionals.chimpTrouble(false, true));
+        System.out.println(condition.chimpTrouble(true, true));
+        System.out.println(condition.chimpTrouble(false, false));
+        System.out.println(condition.chimpTrouble(true, false));
+        System.out.println(condition.chimpTrouble(false, true));
         System.out.println("\n");
 
         // Question 2
         System.out.println("Question 2: \n");
-        System.out.println(conditionals.negPos(1, -1, false));
-        System.out.println(conditionals.negPos(-1, 1, false));
-        System.out.println(conditionals.negPos(1, 1, false));
-        System.out.println(conditionals.negPos(-1, -1, false));
-        System.out.println(conditionals.negPos(-4, -5, true));
+        System.out.println(condition.negPos(1, -1, false));
+        System.out.println(condition.negPos(-1, 1, false));
+        System.out.println(condition.negPos(1, 1, false));
+        System.out.println(condition.negPos(-1, -1, false));
+        System.out.println(condition.negPos(-4, -5, true));
+        System.out.println(condition.negPos(-4, 5, true));
+        System.out.println(condition.negPos(4, 5, true));
+        System.out.println(condition.negPos(4, -5, true));
+        System.out.println("\n");
+
+        // Question 3
+        System.out.println("Question 3: \n");
+        System.out.println(condition.pickUpPhone(false, false, true));
+        System.out.println(condition.pickUpPhone(true, true, true));
+        System.out.println(condition.pickUpPhone(true, true, false));
+        System.out.println(condition.pickUpPhone(false, true, false));
+        System.out.println(condition.pickUpPhone(true, false, false));
+        System.out.println(condition.pickUpPhone(false, false, false));
+        System.out.println("\n");
+
+        // Question 4
+        
+
     }
 }
