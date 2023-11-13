77c4c58ee4ffe0d3271a2cb0c5311fa06a8df90d
AnonLuc <lucasdias@billericak12.com>
2023-11-13 10:21:55 -0500

    Two Spinners (ConEx Pt. 5)

--- Conditionals/TwoSpinners.java
+++ Conditionals/TwoSpinners.java
@@ -1,44 +1,49 @@
 package Conditionals;
 
-import java.util.Random;
+public class TwoSpinners {
+    /**
+     * Precondition: min < max
+     */
+    /*The spin method simulates a spin of a fair spinner.
+     * The method returns a random integer between min and max,
+     * inclusive. Complete the spin method below by assigning
+     * this random integer to result.
+     */
 
-public class TwoSpinners{
- 	/** Precondition: min < max
-  	*/
- 	/*The spin method simulates a spin of a fair spinner.
-  	* The method returns a random integer between min and max,
-  	* inclusive. Complete the spin method below by assigning
-  	* this random integer to result.
-  	*/
- 	public int spin(int min, int max)
- 	{
-     	    int result=0;
-            Random random = new Random();
-            result = random.nextInt((max - min) + 1) + min;
-    	    return result;
- 	}
+    int i = 0;
 
- 	/** Simulates one round of the game as described in part (b).
-  	*/
- 	public void playRound(){
-            int player1 = spin(1, 10);
-            int player2 = spin(1, 10);
-            System.out.println("Player 1: " + player1 + " points");
-            System.out.println("Player 2: " + player2 + " points");
-            if (player1 == player2)
-                System.out.println("Draw");
-            else if (player1 > player2)
-                System.out.println("Player 1 wins \n");
-            else
-                System.out.println("Player 2 wins \n");
+    public int spin(int min, int max) { // Chooses a random number between min and max
+        int result = 0;
+        result = (int) (Math.random()*(max-min))+min;
+        return result;
     }
 
- 	public static void main(String[] args) {
+    /**
+     * Simulates one round of the game as described in part (b).
+     */
+    public void playRound() {
+        int player = spin(1,10); // Declare the spinners
+        int computer = spin(1,10);
+
+        if(player > computer){ // If the player's spinner is > than the computer's spinner
+            System.out.println("Player Won: " + (player - computer) + " points \n"); // Print the difference between the two 
+        } else if(player < computer){ // If the computer's spinner is > than the player's spinner
+            System.out.println("Computer Won: " + (player - computer) + " points \n "); // Print the difference again
+        } else if (player == computer){ // If the player's spinner is = to the computer's spinner
+            if(i < 2){ // If the round is less than 2
+                i++; // Increment the round
+                playRound(); // Play another round
+            } else { // If the round is greater than 2
+                System.out.println("Draw"); // Print "Draw" 
+            } 
+
+        }
+    }
+
+    public static void main(String[] args) { // Main method
         TwoSpinners ds = new TwoSpinners();
-     	      for(int i = 0; i < 10; i++){    //This will save you time by running playground 10 times
-         		ds.playRound();
-     	      }
- 	  }
-
- }
-
+        for (int i = 0; i < 10; i++) {
+            ds.playRound();
+        }
+    }
+}
