a51379862886f27785ad73bf6e2a021cf80ad800
AnonLuc <lucasdias@billericak12.com>
2023-11-21 10:11:23 -0500

    Crabs Game (Complete)

--- Crabs Game/Die.java
+++ Crabs Game/Die.java
@@ -1,28 +1,13 @@
 public class Die {
 
-    private int numDots;  // number of dots on the die's face
-    private int numFaces; // number of faces on the die
-
-    public Die(int faces) { // constructor, sets number of dots to 1 and number of faces to the given value
-        numDots = 1;
-        numFaces = faces;
-    }
-
-    public Die() {
-        numDots = 1;
-        numFaces = 6;
-    }
-
+    int numDots; // number of faces
+    
     public void roll() { // rolls the die to randomly get a value between 1 and the number of faces
-        numDots = (int) (Math.random() * numFaces) + 1;
+        numDots = (int) (Math.random() * 6) + 1; 
     }
 
     public int getNumDots() { // returns number of dots
         return numDots;
-    }
-
-    public int getNumFaces() { // returns number of faces
-        return numFaces;
     }
     
     public static void main(String[] args) { // main for testing
