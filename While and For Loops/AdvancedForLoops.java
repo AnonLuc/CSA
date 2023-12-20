39bcc5dd9f6ada4947035a0a6a0fd699cc64fba3
AnonLuc <lucasdias@billericak12.com>
2023-12-20 10:32:21 -0500

    AdvForLoops (WIP)

--- While and For Loops/AdvancedForLoops.java
+++ While and For Loops/AdvancedForLoops.java
@@ -17,13 +17,22 @@ public class AdvancedForLoops {
         }
     }
     // Question 2
-
+    
+    public static void alternate(int num){
+        for(int i = 0; i <= num; i++){ // make "num" rows
+            for(int j = 0; j < i; j++){ // print numbers
+                System.out.print(i);
+            }
+        }
+    }
+            
 
     // Main
     public static void main(String[] args) {
         // Question 1
         System.out.println("Question 1: \n"); eights();
         // Question 2
-        System.out.println("\nQuestion 2: \n");
+        System.out.println("\nQuestion 2: \n");  
+        alternate(5);
     }
 }
