8b2ce0816519709087f0490199bed671d2e97536
AnonLuc <lucasdias@billericak12.com>
2023-12-20 09:44:38 -0500

    ForLoops (1-5)

--- While and For Loops/ForLoops.java
+++ While and For Loops/ForLoops.java
@@ -36,9 +36,29 @@ public class ForLoops {
             System.out.println();
         }
     }
+    public static void dollarAndStars(){
+        for(int i = 7; i > 0; i--){
+            for(int s = 0; s < 2 * (7 - i); s++){
+                System.out.print("*");
+            }
+            for(int j = 0; j < i; j++){
+                System.out.print("$");
+            }
+            for(int z = 0; z < i * 2; z++){
+                System.out.print("*");
+            }
+            for(int a = 0; a < i; a++){
+                System.out.print("$");
+            }
+            for(int b = 0; b < 2 * (7 - i); b++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
     public static void main(String[] args) {
         // Question 1:
-        System.out.println("Question 1: \n");
+        System.out.println("Question 1 \n");
         System.out.println(addEvens(10));
         System.out.println(addEvens(100));
         System.out.println(addEvens(1000));
@@ -56,6 +76,10 @@ public class ForLoops {
         // Question 4:
         System.out.println("Question 4: \n");
         sillyNumbers2();
+        System.out.println("\n");
+        // Question 5:
+        System.out.println("Question 5: \n");
+        dollarAndStars();
         System.out.println("\n");
     }
 
