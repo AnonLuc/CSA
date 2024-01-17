204f1a6a97d8d9807de959e6114d77ae72e3ce44
AnonLuc <lucasdias@billericak12.com>
2024-01-17 09:50:54 -0500

    StringsEx (1-5)

--- Strings/StringEx.java
+++ Strings/StringEx.java
@@ -15,12 +15,27 @@ public class StringEx {
         return s.replace("0", "2").replace("1", "0").replace("2", "1"); // replaces 0 with 2, 1 with 0, and 2 with 1, since 2 is not in the string, it will not be replaced
     }
     // Question 4
-    public static String dateString(String s){
+    public static String convertDate(String s){
         int slash1 = s.indexOf("/"); // finds first slash
         int slash2 = s.indexOf("/", slash1 + 1); // finds second slash
         String month = s.substring(0, slash1); // finds month
         String day = s.substring(slash1 + 1, slash2); // finds day
         String year = s.substring(slash2 + 1); // finds year
+        return day + "-" + month + "-" + year; // returns day, month, and year
+    }
+    // Question 5
+    public static String convertDate2(String s){
+        int slash1 = s.indexOf("/"); // finds first slash
+        int slash2 = s.indexOf("/", slash1 + 1); // finds second slash
+        String month = s.substring(0, slash1); // finds month
+        String day = s.substring(slash1 + 1, slash2); // finds day
+        String year = s.substring(slash2 + 1); // finds year
+        if (month.length() == 1){ // if month is 1 digit
+            month = "0" + month; // adds 0 to the front of month
+        }
+        if (day.length() == 1){ // if day is 1 digit
+            day = "0" + day; // adds 0 to the front of day
+        }
         return day + "-" + month + "-" + year; // returns day, month, and year
     }
     // Main
@@ -44,7 +59,13 @@ public class StringEx {
         System.out.println("\n");
         // Question 4
         System.out.println("Question 4: \n");
-        System.out.println("04/20/2014 becomes " + dateString("04/20/2014"));
+        System.out.println("04/20/2014 becomes " + convertDate("04/20/2014"));
         System.out.println("\n");
+        // Question 5
+        System.out.println("Question 5: \n");
+        System.out.println("04/20/2014 becomes " + convertDate2("04/20/2014"));
+        System.out.println("4/20/2014 becomes " + convertDate2("4/20/2014"));
+        System.out.println("04/2/2014 becomes " + convertDate2("04/2/2014"));
+        System.out.println("4/2/2014 becomes " + convertDate2("4/2/2014"));
     }
 }
