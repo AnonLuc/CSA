c71454c33315462669db394068a010761658ea6c
AnonLuc <lucasdias@billericak12.com>
2024-01-25 09:39:03 -0500

    CSBS Commit

--- CodeStepByStep/HardStrings.java
+++ CodeStepByStep/HardStrings.java
@@ -11,7 +11,15 @@ public class HardStrings {
         return result;
     }
     // Question 2
-
+    public static String removeDuplicates(String s) {
+        String result = "";
+        for (int i = 0; i < s.length(); i++) {
+            if (i == 0 || s.charAt(i) != s.charAt(i - 1)) {
+                result += s.charAt(i);
+            }
+        }
+        return result;
+    }
     public static void main(String[] args) {
         System.out.println(switchPairs("hello there"));
     }
