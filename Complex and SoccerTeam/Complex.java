06c960108f0d36c2272a0c7e635a192a4c71a5a9
AnonLuc <lucasdias@billericak12.com>
2024-02-08 10:02:43 -0500

    Complex (Complete)

--- Complex and SoccerTeam/Complex.java
+++ Complex and SoccerTeam/Complex.java
@@ -1,39 +1,39 @@
 public class Complex {
 
-    private double real;
+    private double real; 
     private double imag;
 
     public Complex(double real, double imag) {
-        this.real = real;
-        this.imag = imag;
+        this.real = real; // this is used to refer to the current object
+        this.imag = imag; 
     }
 
     public Complex(double real) {
-        this(real, 0.0);
+        this(real, 0.0); 
     }
 
     public String toString() {
-        return real + " + " + imag + "i";
+        return real + " + " + imag + "i"; // i is the imaginary unit
     }
 
     public double abs() {
-        return Math.sqrt(real * real + imag * imag);
+        return Math.sqrt(real * real + imag * imag); // sqrt == square root
     }
 
     public Complex add(Complex other) {
-        return new Complex(real + other.real, imag + other.imag);
+        return new Complex(real + other.real, imag + other.imag); // adding the real and imaginary parts of the two complex numbers
     }
 
     public static void main(String[] args) {
-        Complex c1 = new Complex(0.0, 2.0);
-        Complex c2 = new Complex(3.0);
-        Complex c3 = new Complex(-3.0, 5.0);
-        Complex c4 = new Complex(4.0, -2.0);
-        Complex c5 = new Complex(3.0, 5.0);
-        Complex c6 = new Complex(4.0, -2.0);
-        System.out.println(c1.add(c2));
-        System.out.println(c3.add(c4));
-        System.out.println(c5.add(c6));
+        Complex c1 = new Complex(0.0, 2.0); // 0 + 2i
+        Complex c2 = new Complex(3.0); // 3 + 0i
+        Complex c3 = new Complex(-3.0, 5.0); // -3 + 5i
+        Complex c4 = new Complex(4.0, -2.0); // 4 - 2i
+        Complex c5 = new Complex(3.0, 5.0); // 3 + 5i
+        Complex c6 = new Complex(4.0, -2.0); // 4 - 2i
+        System.out.println(c1.add(c2)); // 3 + 2i (output)
+        System.out.println(c3.add(c4)); // 1 + 3i (output)
+        System.out.println(c5.add(c6)); // 7 + 3i (output)
 
 
     }
